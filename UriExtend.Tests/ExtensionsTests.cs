84467e267f55e7922e122834a38c1cd49b3a7b06
Poul Foged Nielsen <pf@monzoom.net>
2015-06-20 00:51:47 +0200

    Project created

---
+++ UriExtend.Tests/ExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UriExtend.Tests
+{
+    [TestFixture]
+    public class ExtensionsTests
+    {
+        private static object[] _testCases =
+        {
+            new object[] {new Uri("/", UriKind.Relative), new { Animal = "cat" }, new Uri("/?Animal=cat", UriKind.Relative) },
+            new object[] {new Uri("/?a=b&c=d", UriKind.Relative), new { Animal = "cat" }, new Uri("/?a=b&c=d&Animal=cat", UriKind.Relative) },
+            new object[] {new Uri("?a=b&c=d", UriKind.Relative), new { Animal = "cat" }, new Uri("?a=b&c=d&Animal=cat", UriKind.Relative) },
+            new object[] {new Uri("/#fragment", UriKind.Relative), new { Animal = "cat" }, new Uri("/?Animal=cat#fragment", UriKind.Relative) },
+            new object[] {new Uri("/dir1/dir2/file?a=b&c=d", UriKind.Relative), new { Animal = "cat" }, new Uri("/dir1/dir2/file?a=b&c=d&Animal=cat", UriKind.Relative) },
+            new object[] {new Uri("/dir1/dir2/file?a=b&c=d#fragment", UriKind.Relative), new { Animal = "cat" }, new Uri("/dir1/dir2/file?a=b&c=d&Animal=cat#fragment", UriKind.Relative) },
+            new object[] {new Uri("http://www.example.com"), new { Animal = "cat" }, new Uri("http://www.example.com/?Animal=cat") },
+            new object[] {new Uri("https://www.example.com"), new { Animal = "cat" }, new Uri("https://www.example.com/?Animal=cat") },
+            new object[] {new Uri("http://www.example.com"), new { Animals = new[] { "cat", "dog", "pigion" } }, new Uri("http://www.example.com/?Animals=cat&Animals=dog&Animals=pigion") },
+            new object[] {new Uri("http://www.example.com?car=bmw"), new { Animal = "cat" }, new Uri("http://www.example.com/?car=bmw&Animal=cat") },
+            new object[] {new Uri("http://www.example.com"), new { Animal = true }, new Uri("http://www.example.com/?Animal=true") },
+            new object[] {new Uri("http://www.example.com:1234"), new { Animal = true }, new Uri("http://www.example.com:1234/?Animal=true") },
+            new object[] {new Uri("http://www.example.com:80"), new { Animal = true }, new Uri("http://www.example.com:80/?Animal=true") },
+            new object[] {new Uri("http://www.example.com"), new { Arg = "with space" }, new Uri("http://www.example.com/?Arg=with+space") },
+            new object[] {new Uri("http://www.example.com"), new { Arg = "with#hash" }, new Uri("http://www.example.com/?Arg=with%23hash") },
+            new object[] {new Uri("http://www.example.com"), new { Arg = "# %" }, new Uri("http://www.example.com/?Arg=%23+%25") },
+            new object[] {new Uri("http://www.example.com"), new { Today = new DateTime(2000, 1, 1) }, new Uri("http://www.example.com/?Today=2000-01-01T00:00:00.0000000") },
+            new object[] {new Uri("http://www.example.com"), new { Number = 42 }, new Uri("http://www.example.com/?Number=42") },
+            new object[] {new Uri("http://www.example.com"), new { Span = TimeSpan.FromMinutes(10)}, new Uri("http://www.example.com/?Span=00:10:00") },
+        };
+
+        [TestCaseSource(typeof(ExtensionsTests), "_testCases")]
+        public void Can_add_query(Uri source, object parameters, Uri expected)
+        {
+            ////Act
+            var result = source
+                .Query(parameters);
+
+            ////Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ UriExtend/Extensions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace UriExtend
+{
+    /// <summary>
+    /// Adds extension methods to <see cref="System.Uri"/>
+    /// </summary>
+    public static class UriExtensions
+    {
+        private static Regex queryPart = new Regex(@"[^\?#]*\??([^#]*)", RegexOptions.Compiled);
+
+        /// <summary>
+        /// Builds the uri's query string
+        /// </summary>
+        /// <param name="url">Uri to add parameter to</param>
+        /// <param name="parameters">An anonymous object which will be converted to query parameters</param>
+        /// <returns></returns>
+        public static Uri Query(this Uri url, object parameters)
+        {
+            var query = ToQueryString(parameters);
+
+            if (url.IsAbsoluteUri)
+            {
+                var uriBuilder = new UriBuilder(url) {Port = url.Authority.EndsWith(url.Port.ToString(CultureInfo.InvariantCulture)) ? url.Port : -1};
+                if (string.IsNullOrWhiteSpace(uriBuilder.Query))
+                    uriBuilder.Query = ToQueryString(parameters);
+                else
+                    uriBuilder.Query = string.Format("{0}&{1}", uriBuilder.Query.Substring(1), ToQueryString(parameters));
+
+                return uriBuilder.Uri;
+            }
+            
+            var uriString = queryPart.Replace(url.ToString(), match => match.Value.Contains("?") ? (match.Value + "&" + query) : (match.Value + "?" + query), 1);
+            return new Uri(uriString, UriKind.Relative);
+        }
+
+        private static string ToQueryString(object parameters)
+        {
+            var propertyList = parameters
+                .GetType()
+                .GetProperties()
+                .ToList();
+
+            var namesList = propertyList.Select(p => GetValue(parameters, p)).Where(i => !string.IsNullOrWhiteSpace(i));
+            return string.Join("&", namesList);
+        }
+
+        private static string GetValue(dynamic parameters, PropertyInfo p)
+        {
+            var value = p.GetValue(parameters, null);
+
+            if (value == null)
+                return "";
+
+            if (p.PropertyType == typeof(DateTime))
+                return string.Format("{0}={1}", p.Name, Encode(value.ToString("o")));
+            
+            var enumerable = value as IEnumerable;
+            if ((enumerable == null) || (p.PropertyType == typeof(string)))
+                return string.Format("{0}={1}", p.Name, Encode(value));
+
+            var list = enumerable
+                .Cast<object>()
+                .ToList();
+
+            return string.Join("&", list.Select(d => string.Format("{0}={1}", p.Name, Encode(d))));
+        }
+
+        private static object Encode(object getValue)
+        {
+            if (getValue is string)
+                return HttpUtility.UrlEncode((string) getValue);
+            
+            if (getValue is bool)
+                return getValue.ToString().ToLowerInvariant();
+            
+            return getValue;
+        }
+    }
+}
+
+
